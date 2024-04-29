9ce762e372fd4df7a7974cf658dde51a8ce18313
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-30 01:08:47 +0600

    implement update functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 //DiscoverWonder
 //pzqE0eoWVv7v2r5z
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epjsucj.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
@@ -24,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,47 +33,73 @@ async function run() {
     // const database = client.db("insertDB");
     // const haiku = database.collection("haiku");
 
-    const touristSpotCollection = client.db('touristSpotDB').collection('touristSpot');
+    const touristSpotCollection = client
+      .db("touristSpotDB")
+      .collection("touristSpot");
 
-    app.get('/touristSpot', async(req, res) => {
+    app.get("/touristSpot", async (req, res) => {
       const cursor = touristSpotCollection.find();
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
+    // app.patch('/touristSpot', async(req, res) => {
 
-    app.get('/touristSpot/:id', async(req, res) => {
-           const id = req.params.id;
-           const query = {_id: new ObjectId(id)}
-           const result = await touristSpotCollection.findOne(query);
-           res.send(result);
-    })
+    // })
 
+    app.put("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedSpot = req.body;
+      const touristSpots = {
+        $set: {
+          name: updatedSpot.name,
+          countryNames: updatedSpot.countryNames,
+          location: updatedSpot.location,
+          averageCost: updatedSpot.averageCost,
+          seasonality: updatedSpot.seasonality,
+          travelTime: updatedSpot.travelTime,
+          totaVisitorsPerYear: updatedSpot.totaVisitorsPerYear,
+          email: updatedSpot.email,
+          photo: updatedSpot.photo,
+          description: updatedSpot.description,
+        },
+      };
+      const result = await touristSpotCollection.updateOne(
+        filter,
+        touristSpots,
+        options
+      );
+      res.send(result);
+    });
 
+    app.get("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.post('/touristSpot', async(req, res) => {
+    app.post("/touristSpot", async (req, res) => {
       const newTouristSpot = req.body;
       console.log(newTouristSpot);
       const result = await touristSpotCollection.insertOne(newTouristSpot);
       res.send(result);
-    })
+    });
 
-    app.patch('/touristSpot', async(req, res) => {
-      
-    })
-
-
-    app.delete('/touristSpot/:id', async(req, res) => {
-          const id = req.params.id;
-          const query = {_id: new ObjectId(id)};
-          const result = await touristSpotCollection.deleteOne(query);
-          res.send(result)
-    })
-
+    app.delete("/touristSpot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristSpotCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -83,13 +107,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Disconver Wonder is running')
-})
+app.get("/", (req, res) => {
+  res.send("Disconver Wonder is running");
+});
 
 app.listen(port, () => {
-  console.log(`Discover Wonder is Running on Port ${port}`)
-})
+  console.log(`Discover Wonder is Running on Port ${port}`);
+});

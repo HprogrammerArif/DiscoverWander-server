27778c4043fe6f087a0cfdf8ed4230bc0cfe778e
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-29 13:22:18 +0600

    added delete functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -43,11 +43,29 @@ async function run() {
       res.send(result)
     })
 
+
+    app.get('/touristSpot/:id', async(req, res) => {
+           const id = req.params.id;
+           const query = {_id: new ObjectId(id)}
+           const result = await touristSpotCollection.findOne(query);
+           res.send(result);
+    })
+
+
+
     app.post('/touristSpot', async(req, res) => {
       const newTouristSpot = req.body;
       console.log(newTouristSpot);
       const result = await touristSpotCollection.insertOne(newTouristSpot);
       res.send(result);
+    })
+
+
+    app.delete('/touristSpot/:id', async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id)};
+          const result = await touristSpotCollection.deleteOne(query);
+          res.send(result)
     })
 
 

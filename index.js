48acda38903741ac689a6b427d79927232580e3e
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-30 11:52:15 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({origin:["http://localhost:5173","https://discoverwander-16ac4.web.app"]}));
 app.use(express.json());
+
+//app.use (cors({origin:["localhost url","live link url"]}))
 
 //DiscoverWonder
 //pzqE0eoWVv7v2r5z
@@ -28,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     // const database = client.db("insertDB");
     // const haiku = database.collection("haiku");
@@ -96,7 +98,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
